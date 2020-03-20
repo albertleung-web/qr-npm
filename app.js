5ad1938ed042262c1ecaf1f74e5fd2849908431d
wk-leung <wk.leung@samsung.com>
2020-03-20 10:31:41 -0700

    listen to all ip

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const http = require('http');
 const url = require('url');
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
 
 const template = "BEGIN:VCALENDAR\n"+
     "VERSION:2.0\n"+
